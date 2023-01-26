948b0976a1aa0b55bc507c0acc319cd874bd94e8
Leonardo <leonardoluni@gmail.com>
2023-01-26 16:40:55 -0300

    Realização das rotas para o perfil

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import mealsIcon from '../images/mealIcon.svg';
 
@@ -6,12 +7,26 @@ export default function Footer() {
   return (
     <div>
       <div>
-        <button data-testid="drinks-bottom-btn">
-          <img src={ drinkIcon } alt="Drink Icon " />
-        </button>
-        <button data-testid="meals-bottom-btn">
-          <img src={ mealsIcon } alt="eals Icon " />
-        </button>
+        <Route
+          render={ ({ history }) => (
+            <button
+              data-testid="drinks-bottom-btn"
+              onClick={ () => { history.push('/drinks'); } }
+            >
+              <img src={ drinkIcon } alt="Drink Icon " />
+            </button>
+          ) }
+        />
+        <Route
+          render={ ({ history }) => (
+            <button
+              data-testid="meals-bottom-btn"
+              onClick={ () => { history.push('/refeições'); } }
+            >
+              <img src={ mealsIcon } alt="Meals Icon " />
+            </button>
+          ) }
+        />
         <h1 data-testid="page-title">Menu Inferior</h1>
 
       </div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import perfilImage from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 export default function Header() {
   return (
     <div>
-      <button data-testid="profile-top-btn">
-        <img src={ perfilImage } alt="Imagem Perfil " />
-      </button>
+      <Route
+        render={ ({ history }) => (
+          <button
+            data-testid="profile-top-btn"
+            onClick={ () => { history.push('/profile'); } }
+          >
+            <img src={ perfilImage } alt="Imagem Perfil " />
+          </button>
+        ) }
+      />
       <button data-testid="search-top-btn">
         <img src={ searchIcon } alt="Search " />
       </button>
