1e2a9edecb91a913ae3c481c158b188cb2ee9cc7
Leonardo <leonardoluni@gmail.com>
2023-01-27 16:35:36 -0300

    Falta apenas o css

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,33 +1,26 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import mealsIcon from '../images/mealIcon.svg';
 
 export default function Footer() {
   return (
-    <div>
-      <div>
-        <Route
-          render={ ({ history }) => (
-            <button
-              data-testid="drinks-bottom-btn"
-              onClick={ () => { history.push('/drinks'); } }
-            >
-              <img src={ drinkIcon } alt="Drink Icon " />
-            </button>
-          ) }
-        />
-        <Route
-          render={ ({ history }) => (
-            <button
-              data-testid="meals-bottom-btn"
-              onClick={ () => { history.push('/meals'); } }
-            >
-              <img src={ mealsIcon } alt="Meals Icon " />
-            </button>
-          ) }
-        />
-
+    <div data-testid="footer">
+      <div className="fixarRodape">
+        <Link to="/drinks">
+          <button
+            src={ drinkIcon }
+            alt="Drink Icon "
+            data-testid="drinks-bottom-btn"
+          />
+        </Link>
+        <Link to="/meals">
+          <button
+            src={ mealsIcon }
+            alt="Meals Icon "
+            data-testid="meals-bottom-btn"
+          />
+        </Link>
       </div>
     </div>
   );
