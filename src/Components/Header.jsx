9d8bed4ac8c89451083cc8882940a791004ef6f7
Nickolas Martinucci <nickolas64vm@gmail.com>
2023-01-31 17:58:27 -0300

    Implementado requisição API ao pesquisar com a search bar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,33 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import SearchBar from './SearchBar';
 import perfilImage from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 export default function Header() {
+  const [isVisible, setIsVisible] = useState(false);
   const { pathname } = useLocation();
-  const nameArray = pathname.split('/');
-  const name = nameArray[1];
-  const test = name[0].toUpperCase() + name.substring(1);
+
+  if (
+    pathname === '/profile'
+  || pathname === '/done-recipes'
+  || pathname === '/favorite-recipes'
+  ) {
+    const firstName = pathname.split('-')[0].replace('/', '');
+    const firstCamelName = firstName[0].toUpperCase() + firstName.substring(1);
+    let lastName = '';
+    let lastCamelName = '';
+
+    if (pathname.split('-').length === 2) {
+      lastName = pathname.split('-')[1].replace('/', '');
+      lastCamelName = lastName[0].toUpperCase() + lastName.substring(1);
+    }
+
+    return (
+      <div>
+        <Link to="/profile">
+          <input
+            type="image"
+            src={ perfilImage }
+            alt="Imagem Perfil "
+            data-testid="profile-top-btn"
+          />
+        </Link>
+        <h1 data-testid="page-title">
+          { lastCamelName ? `${firstCamelName} ${lastCamelName}` : firstCamelName }
+        </h1>
+      </div>
+    );
+  }
+  const name = pathname.replace('/', '');
+  const camelName = name[0].toUpperCase() + name.substring(1);
+
   return (
     <div>
       <Link to="/profile">
         <input
           type="image"
           src={ perfilImage }
-          alt="Imagem Perfil "
+          alt="Imagem Perfil"
           data-testid="profile-top-btn"
         />
       </Link>
-
-      <h1 data-testid="page-title">{ test }</h1>
-      {/* { console.log(test)} */}
+      <h1 data-testid="page-title">{ camelName }</h1>
       <input
         type="image"
         data-testid="search-top-btn"
         src={ searchIcon }
-        alt="Search "
+        alt="Search"
+        onClick={ () => setIsVisible(!isVisible) }
       />
-
+      { isVisible && <SearchBar /> }
     </div>
   );
 }
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,9 +1,99 @@
-import React from 'react';
+import { useContext } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import { SearchRecipesContext } from '../context/SearchRecipesProvider';
 
 export default function SearchBar() {
+  const {
+    search,
+    setSearch,
+    radio,
+    setRadio,
+    fetchRecipes,
+  } = useContext(SearchRecipesContext);
+
+  const history = useHistory();
+  const { pathname } = useLocation();
+
+  const checkPathName = () => {
+    switch (pathname) {
+    case '/meals':
+      return 'idMeal';
+    case '/drinks':
+      return 'idDrink';
+    default:
+      return null;
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const recipes = await fetchRecipes();
+
+    if (recipes.length <= 1) {
+      // `${pathname}/${recipes[0]}`
+      history.push(`${pathname}/${recipes[0][checkPathName()]}`);
+    }
+  };
+
+  const handleChange = ({ target }) => {
+    setSearch(target.value);
+    if (radio === 'firstLetter' && search.length >= 1) {
+      global.alert('Your search must have only 1 (one) character');
+    }
+  };
+
   return (
-    <div>
-      I
-    </div>
+    <form onSubmit={ handleSubmit }>
+      <input
+        type="text"
+        data-testid="search-input"
+        placeholder="Search"
+        value={ search }
+        onChange={ handleChange }
+      />
+      <label htmlFor="ingredient-search-radio">
+        <input
+          type="radio"
+          id="ingredient-search-radio"
+          data-testid="ingredient-search-radio"
+          name="search-bar-radio"
+          value={ radio }
+          checked={ radio === 'ingredient' }
+          onChange={ () => setRadio('ingredient') }
+        />
+        Ingredient
+      </label>
+      <label htmlFor="name-search-radio">
+        <input
+          type="radio"
+          id="name-search-radio"
+          data-testid="name-search-radio"
+          name="search-bar-radio"
+          value={ radio }
+          checked={ radio === 'name' }
+          onChange={ () => setRadio('name') }
+        />
+        Name
+      </label>
+      <label htmlFor="first-letter-search-radio">
+        <input
+          type="radio"
+          id="first-letter-search-radio"
+          data-testid="first-letter-search-radio"
+          name="search-bar-radio"
+          value={ radio }
+          checked={ radio === 'firstLetter' }
+          onChange={ () => setRadio('firstLetter') }
+        />
+        First letter
+      </label>
+      <button
+        type="submit"
+        data-testid="exec-search-btn"
+        disabled={ (!search) }
+      >
+        SEARCH
+      </button>
+    </form>
   );
 }
